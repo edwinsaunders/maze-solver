b8048ce0b8779c0a37f6f82a9ad66b2da39a896f
edwinsaunders <edwinsaunders36@gmail.com>
2024-11-28 12:38:32 -0500

    draw line implemented

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from window import Window
+from graphics import Point, Line
 
 def main():
 	win = Window(800, 600)
+
+	point1 = Point(2, 3)
+	point2 = Point(40, 50)
+	line1 = Line(point1, point2)
+
+	point1 = Point(20, 30)
+	point2 = Point(40, 100)
+	line2 = Line(point1, point2)
+
+	point1 = Point(500, 3)
+	point2 = Point(0, 75)
+	line3 = Line(point1, point2)
+
+	win.draw_line(line1, "red")
+	win.draw_line(line2, "black")
+	win.draw_line(line3, "red")
+
 	win.wait_for_close()
 
 main()
--- window.py
+++ window.py
@@ -20,3 +20,6 @@ class Window:
 
 	def close(self):
 		self.isRunning = False
+
+	def draw_line(self, line, fill_color):
+		line.draw(self.canvas, fill_color)
